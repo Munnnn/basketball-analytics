9c6f6e081c37ad55e5292bac40480bec99fad590
Munnnn <munchan24@gmail.com>
2025-06-11 18:03:25 -0700

    Update __init__.py
    advanced_manager import

--- team_identification/__init__.py
+++ team_identification/__init__.py
@@ -7,9 +7,11 @@ from .ml_classifier import MLTeamClassifier
 from .color_classifier import ColorBasedClassifier
 from .basketball_rules import BasketballTeamBalancer
 from .crop_manager import CropManager
+from .advanced_manager import AdvancedTeamClassificationManager
 
 __all__ = [
     'TeamClassifierInterface', 'UnifiedTeamClassifier',
     'MLTeamClassifier', 'ColorBasedClassifier',
-    'BasketballTeamBalancer', 'CropManager'
+    'BasketballTeamBalancer', 'CropManager',
+    'AdvancedTeamClassificationManager'
 ]
