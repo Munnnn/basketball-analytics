5aa6099b98c86392ee69475f8f0d8670b89dc6da
Munnnn <munchan24@gmail.com>
2025-07-16 15:16:57 -0700

    fix init.py

--- analytics/events/__init__.py
+++ analytics/events/__init__.py
@@ -2,6 +2,6 @@
 Basketball event detection
 """
 
-from .detector import EventDetector, ShotDetector, ReboundDetectorm, UnifiedEventDetector, BasketballEventDetector
+from .detector import EventDetector, ShotDetector, ReboundDetector, UnifiedEventDetector, BasketballEventDetector
 
 __all__ = ['EventDetector', 'ShotDetector', 'ReboundDetector', 'UnifiedEventDetector', 'BasketballEventDetector']
