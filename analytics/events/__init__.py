c79ae603af668acfa465059d095703108740d3f4
Munnnn <munchan24@gmail.com>
2025-07-16 15:03:08 -0700

    added missing classes in init.py

--- analytics/events/__init__.py
+++ analytics/events/__init__.py
@@ -4,4 +4,4 @@ Basketball event detection
 
 from .detector import EventDetector, ShotDetector, ReboundDetector
 
-__all__ = ['EventDetector', 'ShotDetector', 'ReboundDetector']
+__all__ = ['EventDetector', 'ShotDetector', 'ReboundDetector', 'UnifiedEventDetector', 'BasketballEventDetector']
